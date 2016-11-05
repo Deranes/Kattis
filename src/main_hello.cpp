e39fd595dbed77fcd883cafff369909220a1d871
Ola Enberg <deranes21@hotmail.com>
2016-11-05 20:05:18 +0100

    Hello World! #1

---
+++ src/main_hello.cpp
@@ -0,0 +1,14 @@
+//------------------------------------------------------
+//	Problem: https://open.kattis.com/problems/hello
+//	Author: Ola Enberg
+//	Date: 2016-11-05
+//------------------------------------------------------
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+
+	return 0;	// EXIT_SUCCESS
+};
